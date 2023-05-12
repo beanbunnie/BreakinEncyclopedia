a42601d712a846d7646b4c19df10b4f85f9354d1
Magnolia Cheng <magnolia@magnolias-air.mynetworksettings.com>
2023-05-12 19:04:43 -0400

    index fixed

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import { LindyHop, WestAfrican, Kungfu, Capoeira, Home, Jazz } from './pages';
+import { LindyHop, WestAfrican, Kungfu, Capoeira, Home, Jazz } from "./pages";
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Jazz" element={<Jazz />} />
-          <Route path="/Capoeira" element={<Capoeira />} />
-          <Route path="/Kungfu" element={<Kungfu />} />
-          <Route path="/WestAfrican" element={<WestAfrican />} />
-          <Route path="/LindyHop" element={<LindyHop />} />
-        </Routes>
-    </BrowserRouter>
+        <Route path="/" element={<Home />} />
+        <Route path="/Jazz" element={<Jazz />} />
+        <Route path="/Capoeira" element={<Capoeira />} />
+        <Route path="/Kungfu" element={<Kungfu />} />
+        <Route path="/WestAfrican" element={<WestAfrican />} />
+        <Route path="/LindyHop" element={<LindyHop />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
