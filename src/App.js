2870037c08e7b399fc74ac6c1f42fbd2013ed295
Magnolia Cheng <magnolia@magnolias-air.mynetworksettings.com>
2023-05-12 20:35:13 -0400

    Attempting to fix links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 // Currently functional using hashrouter
 function App() {
   return (
-    <HashRouter basename="/">
+    <HashRouter basename="/BreakinEncyclopedia">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/Jazz" element={<Jazz />} />
