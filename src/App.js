3bdb9ab33c0815efd3021903e5893d0c357bba9f
Magnolia Cheng <magnolia@magnolias-air.mynetworksettings.com>
2023-05-12 20:27:11 -0400

    index fixed

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <HashRouter basename="/">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Jazz" element={<Jazz />} />
-          <Route path="/Capoeira" element={<Capoeira />} />
-          <Route path="/Kungfu" element={<Kungfu />} />
-          <Route path="/WestAfrican" element={<WestAfrican />} />
-          <Route path="/LindyHop" element={<LindyHop />} />
-        </Routes>
-      </HashRouter>
-    </BrowserRouter>
+    <HashRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Jazz" element={<Jazz />} />
+        <Route path="/Capoeira" element={<Capoeira />} />
+        <Route path="/Kungfu" element={<Kungfu />} />
+        <Route path="/WestAfrican" element={<WestAfrican />} />
+        <Route path="/LindyHop" element={<LindyHop />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
