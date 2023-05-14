c1e057516b72763c553da96513de786c8e21a809
Magnolia Cheng <magnolia@magnolias-air.mynetworksettings.com>
2023-05-14 02:17:24 -0400

    links

--- src/components/SubNav.jsx
+++ src/components/SubNav.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link as LScroll, animateScroll as scroll } from "react-scroll";
+import { Link, animateScroll as scroll } from "react-scroll";
 import WebFont from "webfontloader";
 
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
