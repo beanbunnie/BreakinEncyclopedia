d9fae64d689ea8cf12b06b73cc190f3e04b68e07
Magnolia Cheng <magnolia@magnolias-air.mynetworksettings.com>
2023-05-14 01:37:23 -0400

    links

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import { BrowserRouter, HashRouter, Router, Route } from "react-router-dom";
 function App() {
   return (
     <HashRouter basename="/">
-      <Router>
-        <Route path="/" element={<Home />} />
-        <Route path="/Jazz" element={<Jazz />} />
-        <Route path="/Capoeira" element={<Capoeira />} />
-        <Route path="/Kungfu" element={<Kungfu />} />
-        <Route path="/WestAfrican" element={<WestAfrican />} />
-        <Route path="/LindyHop" element={<LindyHop />} />
-      </Router>
+      <Route path="/" element={<Home />} />
+      <Route path="/Jazz" element={<Jazz />} />
+      <Route path="/Capoeira" element={<Capoeira />} />
+      <Route path="/Kungfu" element={<Kungfu />} />
+      <Route path="/WestAfrican" element={<WestAfrican />} />
+      <Route path="/LindyHop" element={<LindyHop />} />
     </HashRouter>
   );
 }
